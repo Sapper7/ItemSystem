4ead1f97b27001aab54b6f4dd04cd0f12ef83bb5
Jonathan Pool <reddevpool@gmail.com>
2015-05-24 22:01:41 -0700

    Started to build IS

---
+++ Item System/Assets/PuddleSoft/Item System/Scripts/ISObject.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ISObject : IISObject {
+	[SerializeField]Sprite _icon;
+	[SerializeField]string _name;
+	[SerializeField]int _value;
+	[SerializeField]int _burden;
+	[SerializeField]ISQuality quality;
+
+	#region IISObject implementation
+
+	public string ISName {
+		get {
+			throw new System.NotImplementedException ();
+		}
+		set {
+			throw new System.NotImplementedException ();
+		}
+	}
+
+	public int ISValue {
+		get {
+			throw new System.NotImplementedException ();
+		}
+		set {
+			throw new System.NotImplementedException ();
+		}
+	}
+
+	public Sprite ISIcon {
+		get {
+			throw new System.NotImplementedException ();
+		}
+		set {
+			throw new System.NotImplementedException ();
+		}
+	}
+
+	public int ISBurden {
+		get {
+			throw new System.NotImplementedException ();
+		}
+		set {
+			throw new System.NotImplementedException ();
+		}
+	}
+
+	public ISQuality ISquality {
+		get {
+			throw new System.NotImplementedException ();
+		}
+		set {
+			throw new System.NotImplementedException ();
+		}
+	}
+
+	#endregion
+
+
+}
---
+++ Item System/Assets/PuddleSoft/Item System/Scripts/Interfaces/IISObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IISObject {
+	//name
+	//value - gold
+	//icon
+	//burden
+	//quality level
+	string ISName { get; set;}
+	int ISValue { get; set;}
+	Sprite ISIcon { get; set;}
+	int ISBurden { get; set;}
+	ISQuality ISquality { get; set;}
+
+	//equipable
+	//questItem flag
+	//durabilit
+	//takedamag
+	//prefab
+}
